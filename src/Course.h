03382e0dc4b90d3f396ac6b836d0a359892d2e35
MatinQurban <matinqurban@gmail.com>
2023-09-21 15:33:27 -0700

    Created src and header filees for classes

---
+++ src/Course.h
@@ -0,0 +1,12 @@
+// Header file for Course class
+/*1. A course has an instructor, name and description (e.g. Homi Bodhanwala, CS 1C, Intro to Computer Science III).
+
+2. Each course contains several students and assignments.
+
+3. A course supports:
+
+a) Printing the student roster (names and id numbers of the students)
+
+b) Printing the assignment list (names and descriptions of assignments)
+
+c) Displaying the final grades of each student (student id, student name, letter grade)*/
