c043bdca7686e1f670b70b95a3df2a2b2b5b5bf6
MatinQurban <matinqurban@gmail.com>
2023-09-21 16:49:12 -0700

    Header file for course class.

--- src/Course.h
+++ src/Course.h
@@ -10,3 +10,31 @@ a) Printing the student roster (names and id numbers of the students)
 b) Printing the assignment list (names and descriptions of assignments)
 
 c) Displaying the final grades of each student (student id, student name, letter grade)*/
+
+
+#ifndef COURSE_H_
+#define COURSE_H_
+
+#include <string>
+#include "Student.h"
+#include "Assignment.h"
+using namespace std;
+
+
+class Course {
+public:
+    Course();
+    Course(string instructor, string CourseTitle, string CourseDescription);
+
+    void addStudent(Student student);
+    void addAssignment(Assignment assignment);
+
+    void printRoster();
+    void printAssignments();
+
+
+
+};
+
+
+#endif // COURSE_H_
