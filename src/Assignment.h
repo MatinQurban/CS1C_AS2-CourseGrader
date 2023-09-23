b7a732769a7a2de50cd794e0e7bb5d4a4e175e5e
MatinQurban <matinqurban@gmail.com>
2023-09-22 22:23:26 -0700

    Header file for Assignments class

--- src/Assignment.h
+++ src/Assignment.h
@@ -1,1 +1,32 @@
 //Header file for Assignment class
+// 4. Assignments have a name, description, and total points possible.
+
+
+#ifndef ASSIGNMENT_H_
+#define ASSIGNMENT_H_
+
+#include <string>
+using namespace std;
+
+class Assignment {
+public:
+    Assignment();
+    Assignment(string name, string description, int totalPointsPossible);
+
+    string getName();
+    string getDescription();
+    int getTotalPointsPossible();
+
+    void setName(string name);
+    void setDescription(string description);
+    void setTotalPointsPossible(int totalPointsPossible);
+
+private:
+    string name;
+    string description;
+    int totalPointsPossible;
+};
+
+
+
+#endif // ASSIGNMENT_H_
